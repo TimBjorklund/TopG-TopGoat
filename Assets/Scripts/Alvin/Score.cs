14fe72bdf8e5772e9511cf1ba2ccef3f612e379d
D9alle <alvin.nygren@elev.ga.lbs.se>
2023-01-13 09:47:26 +0100

    Update Score.cs

--- Assets/Scripts/Alvin/Score.cs
+++ Assets/Scripts/Alvin/Score.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+
+    public int GretaScore;
+
+    public Text scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "GretaScore";
     }
+
 }
