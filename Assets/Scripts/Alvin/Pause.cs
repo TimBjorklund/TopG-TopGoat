ed604366af374cf0126eab8bcf3f248674fcbdd7
D9alle <alvin.nygren@elev.ga.lbs.se>
2023-01-13 14:51:44 +0100

    bra

--- Assets/Scripts/Alvin/Pause.cs
+++ Assets/Scripts/Alvin/Pause.cs
@@ -6,12 +6,12 @@ using UnityEngine.SceneManagement;
 public class Pause : MonoBehaviour
 {
 
-    public static bool GameIsPaused = false;
+    public static bool GameIsPaused = false; // Här gör jag en bool som har värdet False.
 
-    public GameObject PausedUI;
+    public GameObject PausedUI; // Här gör jag ett gameobject där jag lägger in canvasen i (där scriptsen finns)
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)) // Om escape blir tryckt så kopplas paused med resume some en knapp och else paused
         {
             if (GameIsPaused)
             {
@@ -23,14 +23,14 @@ public class Pause : MonoBehaviour
             }
         }
     }
-    public void Resume ()
+    public void Resume () // Om boolen aktiveras så e den false o då är tiden 1. samt spelet e pausat
     {
         PausedUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
 
     }
-    void Paused()
+    void Paused() // Om paused så är den true o tiden e 0.
     {
         PausedUI.SetActive(true);
         Time.timeScale = 0f;
