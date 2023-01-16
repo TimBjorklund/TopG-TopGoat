0e57d6544fe0270f51f0ac961a8b39ee7f24df28
D9alle <alvin.nygren@elev.ga.lbs.se>
2023-01-16 11:33:44 +0100

    Update EndGame.cs

--- Assets/Scripts/Alvin/EndGame.cs
+++ Assets/Scripts/Alvin/EndGame.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class EndGame : MonoBehaviour
 {
+    
     void OnTriggerEnter(Collider other)
     {
       
     }
-
+   
     public void WinGame() // Funktion som heter Wingame.
     {
         SceneManager.LoadScene(3); // Laddar Scene 3 i builden.
+    }
+    void die() //funktion som är att dö
+    {
+       
+        SceneManager.LoadScene(4); //ladda game over scene
     }
 }
 //Alvin
