5c206fb17a4f3d222cc87f0d8181542a5ec1f449
TimBjorklund <tim.bjorklund@elev.ga.lbs.se>
2023-01-19 09:38:32 +0100

    Bottle Boss

--- Assets/Scripts/Tim/BossThrow.cs
+++ Assets/Scripts/Tim/BossThrow.cs
@@ -7,10 +7,13 @@ public class BossThrow : MonoBehaviour
     private float timer;
     [SerializeField]
     private GameObject bottlePrefab;
+    private GameObject arm;
+    private GameObject Aim;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -20,7 +23,8 @@ public class BossThrow : MonoBehaviour
         if (timer >= 5)
         {
             timer = 0;
-            Instantiate(bottlePrefab, transform.position, Quaternion.identity);
+            Instantiate(bottlePrefab, arm.transform.position, Quaternion.identity);
+            Aim.transform.LookAt(player.transform.position);
         }
     }
 }
--- Assets/Scripts/Tim/Bottle.cs
+++ Assets/Scripts/Tim/Bottle.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Bottle : MonoBehaviour
 {
+    private Rigidbody2D r2d;
+    Player player;
+    [SerializeField]
+    private GameObject spinning;
+    private Vector3 aimedLocation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        r2d = gameObject.GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<Player>();
+        aimedLocation = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spinning.transform.eulerAngles += new Vector3(0, 0, 400) * Time.deltaTime;
+
     }
 }
