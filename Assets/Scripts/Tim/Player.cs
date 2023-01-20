f31735ffe405f31a330c2aabe945eea3af5915e2
erectus86 <94228433+erectus86@users.noreply.github.com>
2023-01-20 12:01:31 +0100

    ss

--- Assets/Scripts/Tim/Player.cs
+++ Assets/Scripts/Tim/Player.cs
@@ -125,7 +125,7 @@ public class Player : MonoBehaviour
     }
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundLayer);
     }
 }
 // Tim.B
